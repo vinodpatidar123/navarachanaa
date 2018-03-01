2e1a29032db59434ec6ab5dc059fa66384fce8cc
Namit Doshi <35694432+namitdoshi@users.noreply.github.com>
2018-03-01 23:38:54 +0530

    initial commit

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+
+  var options = [
+    {selector: '.class', offset: 200, callback: 'globalFunction()' },
+    {selector: '.other-class', offset: 200, callback: 'globalFunction()' },
+  ];
+  Materialize.scrollFire(options);
+        
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
