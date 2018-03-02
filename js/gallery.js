154fb3990d0e4baf59cd6298cdfcca9cc059e447
Namit Doshi <35694432+namitdoshi@users.noreply.github.com>
2018-03-02 14:59:14 +0530

    gallery-img-add, bugs-fixed
    Images were added to gallery page and bugs were fixed by creating a new js file(gallery.js) for fancybox.

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+    $(.images).fancybox({
+      selector : '[data-fancybox="group"]',
+      loop     : true
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
--- js/init.js
+++ js/init.js
@@ -10,27 +10,6 @@
     {selector: '.other-class', offset: 200, callback: 'globalFunction()' },
   ];
   Materialize.scrollFire(options);
-  $('.popup-gallery').magnificPopup({
-  delegate: 'a',
-  type: 'image',
-  tLoading: 'Loading image #%curr%...',
-  mainClass: 'mfp-img-mobile',
-  gallery: {
-    enabled: true,
-    navigateByImgClick: true,
-    preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-  },
-  image: {
-    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-    titleSrc: function(item) {
-      return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-    }
-  }
-});
-$(.images).fancybox({
-  selector : '[data-fancybox="group"]',
-  loop     : true
-});
 
 
   }); // end of document ready
